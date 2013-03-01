cfc7f6dcb8743ae55e08b6b132fdc2872f8dfe4b
Steffen Waldherr <waldherr@ist.uni-stuttgart.de>
2013-03-01 17:46:49 +0100

    Fixed a bug in error reporting.

--- scripttool/memoize.py
+++ scripttool/memoize.py
@@ -29,7 +29,7 @@ def filecache(func):
         try:
             arghash = pickle.dumps((args,kwargs))
         except (TypeError, pickle.PickleError) , e:
-            print "Warning: %s in memoizing %s, not using cache." % (type(e).__name__, func.__name)
+            print "Warning: %s in memoizing %s, not using cache." % (type(e).__name__, func.__name__)
             funcres = func(*args, **kwargs)
         else:
             cache = shelve.open(dbname + funchash)
